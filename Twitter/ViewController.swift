9dd560eee71ec969de042a3624e257671bb1c59a
suemen97 <aguse2@illinois.edu>
2016-02-13 21:59:41 -0600

    request token

--- Twitter/ViewController.swift
+++ Twitter/ViewController.swift
@@ -24,6 +24,14 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func onLogin(sender: AnyObject) {
+        TwitterClient.sharedInstance.requestSerializer.removeAccessToken()
+        TwitterClient.sharedInstance.fetchRequestTokenWithPath("oauth/request_token", method: "GET", callbackURL: NSURL(string: "cptwitterdemo://oauth"), scope: nil, success: {(requestToken: BDBOAuth1Credential!) -> Void in
+            var authURL = NSURL(string: "https://api.twitter.com/oauth/authorize?oauth_token=\(requestToken.token)")
+            UIApplication.sharedApplication().openURL(authURL!)
+            }) {(error: NSError!) -> Void in
+        }
+    }
 
 }
 
