4ec892dfd88257028fb884be741434eacffacc4c
suemen97 <aguse2@illinois.edu>
2016-02-23 22:48:59 -0600

    adding twitter cell

---
+++ Twitter/TweetCell.swift
@@ -0,0 +1,24 @@
+//
+//  TweetCell.swift
+//  Twitter
+//
+//  Created by Nuraini Aguse on 2/23/16.
+//  Copyright © 2016 Nuraini Aguse. All rights reserved.
+//
+
+import UIKit
+
+class TweetCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
